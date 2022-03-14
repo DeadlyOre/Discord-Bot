a71ac9ea3ba97438cd45156a15866aa1dc072111
Powell DeHart <Deadly_Ore@outlook.com>
2022-03-13 20:56:49 -0400

    Added better framework, need to re-add join message

--- index.js
+++ index.js
@@ -12,30 +12,48 @@ const client = new Discord.Client({
 
 })
 
-client.on("ready", () => {
+let bot = {
+    client,
+    prefix: "g.",
+    owners: [process.env.OWNERID]
+}
 
-    console.log(`Logged in as ${client.user.tag}`)
 
-})
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
 
-client.on("messageCreate", (message) => {
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
 
-    if (message.content == "hi") {
-        message.reply("Hello World!")
-    }
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
 
-})
+module.exports = bot
 
-const welcomeChannelId = "952674331098497074"
-
-client.on("guildMemberAdd", async (member) => {
-
-    const img = await generateImage(member)
-    member.guild.channels.cache.get(welcomeChannelId).send({
-        content: `<@${member.id}> Welcome to the server!`,
-        files: [img]
-    })
-
-})
+// client.on("ready", () => {
+//
+//     console.log(`Logged in as ${client.user.tag}`)
+//
+// })
+//
+// client.on("messageCreate", (message) => {
+//
+//     if (message.content == "hi") {
+//         message.reply("Hello World!")
+//     }
+//
+// })
+//
+// const welcomeChannelId = "952674331098497074"
+//
+// client.on("guildMemberAdd", async (member) => {
+//
+//     const img = await generateImage(member)
+//     member.guild.channels.cache.get(welcomeChannelId).send({
+//         content: `<@${member.id}> Welcome to the server!`,
+//         files: [img]
+//     })
+//
+// })
 
 client.login(process.env.TOKEN)
